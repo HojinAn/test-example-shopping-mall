d7ab1405bbee91d1f6f0034bfbddfcddc957b4ae
HojinAn <devian3143@gmail.com>
2024-02-12 00:22:24 +0900

    TextField에 대한 테스트 description 작성

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,6 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
+it('className prop으로 설정한 css class가 적용된다.', async () => {
   expect(1).toBe(1);
 });
