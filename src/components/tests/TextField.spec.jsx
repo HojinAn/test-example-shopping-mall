866ecd041c76ec322f097776add478200ed0a258
HojinAn <devian3143@gmail.com>
2024-02-12 00:31:37 +0900

    Arrange 단계 코드 작성

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -7,6 +7,11 @@ import render from '@/utils/test/render';
 it('className prop으로 설정한 css class가 적용된다.', async () => {
   // Arrange - 테스트를 위한 환경 만들기
   // -> className을 가진 TextField를 렌더링
+
+  // render API를 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 렌더링된 DOM 구조가 반영
+  // jsDOM: Node.js에서 사용하기 위해 웹 표준을 자바스크립트로 구현한 것
+  await render(<TextField className="my-class" />);
+
   // Act - 테스트할 동작 발생
   // -> 렌더링에 대한 검증이라서 생략
   // -> 클릭, 메서드 호출, prop 변경 등이 해당됨
