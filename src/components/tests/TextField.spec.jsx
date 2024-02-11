fde10252afe041dac8fed7756b04f86dcba0af41
HojinAn <devian3143@gmail.com>
2024-02-12 00:37:47 +0900

    Assert 단계 코드 작성

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -15,6 +15,16 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
   // Act - 테스트할 동작 발생
   // -> 렌더링에 대한 검증이라서 생략
   // -> 클릭, 메서드 호출, prop 변경 등이 해당됨
+
   // Assert - 올바른 동작이 실행되었는지 검증
   // -> 렌더링 후 DOM에 className이 적용되었는지 확인
+
+  // screen 객체의 getByPlaceholderText 메서드를 사용하여 input 엘리먼트를 찾음
+  // vitest의 expect 함수를 사용하여 기대 결과를 검증
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'my-class',
+  );
+
+  // className이라는 내부 prop이나 state 값을 검증 (X)
+  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 검증 (O) -> 최종적으로 사용자가 보는 결과는 DOM
 });
