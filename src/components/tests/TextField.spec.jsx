20900b707a91c8462d2a9e9a1e55d92a7b95d0b9
HojinAn <devian3143@gmail.com>
2024-02-12 22:38:46 +0900

    placeholder 관련 테스트 코드

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -12,8 +12,6 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
   // jsDOM: Node.js에서 사용하기 위해 웹 표준을 자바스크립트로 구현한 것
   await render(<TextField className="my-class" />);
 
-  screen.debug();
-
   // Act - 테스트할 동작 발생
   // -> 렌더링에 대한 검증이라서 생략
   // -> 클릭, 메서드 호출, prop 변경 등이 해당됨
@@ -23,10 +21,30 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
 
   // screen 객체의 getByPlaceholderText 메서드를 사용하여 input 엘리먼트를 찾음
   // vitest의 expect 함수를 사용하여 기대 결과를 검증
-  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
-    'my-class',
-  );
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+  expect(textInput).toHaveClass('my-class');
 
   // className이라는 내부 prop이나 state 값을 검증 (X)
   // 렌더링되는 DOM 구조가 올바르게 변경되었는지 검증 (O) -> 최종적으로 사용자가 보는 결과는 DOM
+});
+
+// it: test의 alias
+it('기본 placeholder "텍스트를 입력해 주세요."가 렌더링된다.', async () => {
+  // 기대 결과 === 실제 결과 -> 성공
+  // 기대 결과 !== 실제 결과 -> 실패
+  await render(<TextField />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  expect(textInput).toBeInTheDocument();
+  // 단언(assertion) -> 테스트가 통과하기 위한 조건 -> 검증 실행
+});
+
+it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+  const placeholder = '상품명을 입력해주세요.';
+  await render(<TextField placeholder={placeholder} />);
+
+  const textInput = screen.getByPlaceholderText(placeholder);
+
+  expect(textInput).toBeInTheDocument();
 });
