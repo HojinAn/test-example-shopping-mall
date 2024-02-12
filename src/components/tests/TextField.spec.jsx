34bd5e7b143320dc2222e97e9604d30de6906b9c
HojinAn <devian3143@gmail.com>
2024-02-12 22:44:52 +0900

    describe block으로 scope 제한 가능

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -28,23 +28,26 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
   // 렌더링되는 DOM 구조가 올바르게 변경되었는지 검증 (O) -> 최종적으로 사용자가 보는 결과는 DOM
 });
 
-// it: test의 alias
-it('기본 placeholder "텍스트를 입력해 주세요."가 렌더링된다.', async () => {
-  // 기대 결과 === 실제 결과 -> 성공
-  // 기대 결과 !== 실제 결과 -> 실패
-  await render(<TextField />);
+describe('placeholder', () => {
+  // it: test의 alias
+  it('기본 placeholder "텍스트를 입력해 주세요."가 렌더링된다.', async () => {
+    // 기대 결과 === 실제 결과 -> 성공
+    // 기대 결과 !== 실제 결과 -> 실패
+    await render(<TextField />);
 
-  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
-  expect(textInput).toBeInTheDocument();
-  // 단언(assertion) -> 테스트가 통과하기 위한 조건 -> 검증 실행
-});
+    expect(textInput).toBeInTheDocument();
+    // expect 함수가 제공하는 matcher를 사용하여 실행
+    // 단언(assertion) -> 테스트가 통과하기 위한 조건 -> 검증 실행
+  });
 
-it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
-  const placeholder = '상품명을 입력해주세요.';
-  await render(<TextField placeholder={placeholder} />);
+  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+    const placeholder = '상품명을 입력해주세요.';
+    await render(<TextField placeholder={placeholder} />);
 
-  const textInput = screen.getByPlaceholderText(placeholder);
+    const textInput = screen.getByPlaceholderText(placeholder);
 
-  expect(textInput).toBeInTheDocument();
+    expect(textInput).toBeInTheDocument();
+  });
 });
