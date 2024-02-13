41a92414a255fef43afcd66ead4f3dbcb015f4f2
HojinAn <devian3143@gmail.com>
2024-02-13 22:12:01 +0900

    실제 모듈을 모킹 모듈로 대체

--- src/pages/cart/components/tests/EmptyNotice.spec.jsx
+++ src/pages/cart/components/tests/EmptyNotice.spec.jsx
@@ -4,4 +4,11 @@ import React from 'react';
 import EmptyNotice from '@/pages/cart/components/EmptyNotice';
 import render from '@/utils/test/render';
 
+// 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+
+  return original;
+});
+
 it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {});
