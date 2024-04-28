dc978bd21f6990c88265316485b9e20b48ae7c36
안호진 <47904304+HojinAn@users.noreply.github.com>
2024-04-28 18:21:44 +0900

    Chapter 3. Shopping mall unit test practice (#2)
    * shopping-mall-unit-test
    
    * 실제 모듈을 모킹 모듈로 대체
    
    * useNavigate만 스파이 함수로 모킹
    
    * 홈 링크 버튼 클릭하면 홈으로 이동하는지 테스트
    
    * ErrorPage에서 getByRole로 가져와서 클릭
    
    * Mocking 함수 가져와서 navigate(-1) 검증
    
    * NotFoundPage 테스트
    
    * teardown에서 매번 히스토리 초기화 마지막에 모든 구현 초기화
    
    * React hook 테스트 + act 함수 역할
    
    * 일단 debounce용 테스트 description 작성
    
    * debounce 테스트 코드 작성하면서 타이머 모킹도 함
    
    * test 통과 하도록
    
    ---------
    
    Co-authored-by: hanjung <hanjungv@gmail.com>

--- src/utils/test/setupTests.js
+++ src/utils/test/setupTests.js
@@ -1,14 +1,33 @@
+import { setupServer } from 'msw/node';
+
 import '@testing-library/jest-dom';
 // vitest에서는 dom 검증을 위한 matchers를 제공하지 않음. 그래서 jest-dom을 사용하여 확장
+import { handlers } from '@/__mocks__/handlers';
+
+/* msw */
+export const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
 
 // 모킹한 모듈의 히스토리를 초기화
 afterEach(() => {
+  server.resetHandlers();
+  // 모킹된 모의 객체 호출에 대한 히스토리를 초기화
+  // 모킹된 모듈의 구현을 초기화하지는 않는다. -> 모킹된 상태로 유지됨
+  // -> 모킹 모듈 기반으로 작성한 테스트가 올바르게 실행
+  // 반면, 모킹 히스토리가 계속 쌓임(호출 횟수나 인자가 계속 변경) -> 다른 테스트에 영향을 줄 수 있음
   vi.clearAllMocks();
 });
 
 afterAll(() => {
+  // 모킹 모듈에 대한 모든 구현을 초기화
   vi.resetAllMocks();
+  server.close();
 });
+
+vi.mock('zustand');
 
 // https://github.com/vitest-dev/vitest/issues/821
 Object.defineProperty(window, 'matchMedia', {
