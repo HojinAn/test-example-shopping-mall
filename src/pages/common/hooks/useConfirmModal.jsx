85f379aeb6a30fc0e7a95cab37d8b283e541a466
HojinAn <devian3143@gmail.com>
2024-04-28 17:41:54 +0900

    React hook 테스트 + act 함수 역할

--- src/pages/common/hooks/useConfirmModal.jsx
+++ src/pages/common/hooks/useConfirmModal.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+// - 호출 시 initialValue 인자를 지정하지 않은 경우 isModalOpened 상태가 false로 설정
+// - 호출 시 initialValue 인자를 boolean 값으로 지정한 경우 isModalOpened 상태가 해당 값으로 설정
+// - 훅의 toggleIsModalOpened()를 호출하면 isModalOpened 상태가 toggle됨
 const useConfirmModal = (initialValue = false) => {
   const [isModalOpened, setIsModalOpened] = useState(initialValue);
 
